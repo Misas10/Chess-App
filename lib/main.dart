7cf6499235a9b025f81d31554e671a1796a853c3
Misas10 <manmisa2003@gmail.com>
2022-09-01 12:16:32 +0100

    Better Chess Board

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:chess_app/players_timers.dart';
+import 'package:chess_app/player_clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chess_board/flutter_chess_board.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:chess_app/widgets/text_colors.dart';
 
 void main() {
   runApp(const ProviderScope(child: ChessApp()));
@@ -13,10 +14,12 @@ class ChessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       title: 'Chess App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColorDark: primaryColor,
       ),
       home: const MyHomePage(title: 'Chess App'),
     );
@@ -32,28 +35,64 @@ class MyHomePage extends ConsumerStatefulWidget {
   MyHomePageState createState() => MyHomePageState();
 }
 
+final whiteClockProvider = ChangeNotifierProvider(
+    (_) => PlayerTimerNotifier(duration: const Duration(minutes: 5)));
+
+final blackClockProvider = ChangeNotifierProvider(
+    (_) => PlayerTimerNotifier(duration: const Duration(minutes: 5)));
+
 class MyHomePageState extends ConsumerState<MyHomePage> {
+  late ChessBoardController chessBoardController;
+
+  @override
+  void initState() {
+    super.initState();
+    chessBoardController = ChessBoardController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    chessBoardController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    ChessBoardController chessBoardController = ChessBoardController();
+    // White Clock Provider
+    Clock whiteClock = Clock(ref,
+        provider: whiteClockProvider,
+        piecesColor: Color.WHITE,
+        chessBoardController: chessBoardController);
+
+    // Black Clock Provider
+    Clock blackClock = Clock(ref,
+        provider: blackClockProvider,
+        piecesColor: Color.BLACK,
+        chessBoardController: chessBoardController);
 
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColorDark,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: labelText, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: appBg,
       ),
-      body: Center(
+      body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
               margin: EdgeInsets.only(
                   top: 10, bottom: MediaQuery.of(context).size.height / 32),
-              // Creates a scrollable list
+              // Creates a scrollable horizontal list
               child: SingleChildScrollView(
                 // Scrolls automatically when a new item is added
                 reverse: true,
                 scrollDirection: Axis.horizontal,
                 // Listens to a value and updates it with the builder
-                // In this case values stored in the chessBoardController variable
+                // In this case, values stored in the chessBoardController variable
                 child: ValueListenableBuilder<Chess>(
                   valueListenable: chessBoardController,
                   builder: ((context, value, child) => Text(chessBoardController
@@ -65,12 +104,53 @@ class MyHomePageState extends ConsumerState<MyHomePage> {
                 ),
               ),
             ),
-            ChessBoard(
-              controller: chessBoardController,
-            ),
-            PlayerTimer(chessBoardController: chessBoardController),
+            _buildChessBoard(whiteClock: whiteClock, blackClock: blackClock),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildChessBoard(
+      {required Clock whiteClock, required Clock blackClock}) {
+    if (chessBoardController.value.turn == Color.WHITE) {
+      whiteClock.start();
+      blackClock.stop();
+    }
+
+    if (chessBoardController.value.turn == Color.BLACK) {
+      blackClock.start();
+      whiteClock.stop();
+    }
+    return Column(
+      children: [
+        _infoRow("Black", blackClock.show()),
+        ChessBoard(
+          controller: chessBoardController,
+        ),
+        _infoRow("White", whiteClock.show()),
+      ],
+    );
+  }
+
+  Padding _infoRow(String playerName, Widget showClock) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            playerName,
+            style: const TextStyle(
+              fontFamily: "Roboto Mono",
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: labelText,
+            ),
+          ),
+          showClock,
+        ],
       ),
     );
   }
