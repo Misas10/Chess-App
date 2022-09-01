ea8f1d12054fb4234c4ef91a750961485f04a8b4
Misas10 <manmisa2003@gmail.com>
2022-09-01 12:21:03 +0100

    Initial commit

---
+++ lib/widgets/text_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color labelText = Color.fromRGBO(180, 180, 180, 1);
+const Color primaryColor = Color.fromRGBO(24, 24, 24, 1);
+const Color appBg = Color.fromRGBO(48, 48, 48, 1);
+
+// Clock colors
+const Color whiteClockActiveBg = Colors.white;
+const Color blackClockActiveBg = Colors.black;
+const Color clockInactiveBg = Color.fromRGBO(29, 29, 29, 1);
+
+const Color whiteClockActiveText = primaryColor;
+const Color whiteClockInactiveText = labelText;
+
+const Color blackClockActiveText = labelText;
+const Color blackClockInactiveText = labelText;
